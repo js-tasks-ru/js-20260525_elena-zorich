cc06e440c383aadfa8244875c23fd25467b4a04a
elzorich <elenazorich@gmail.com>
2026-05-26 13:30:36 +0200

    Omit function

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,6 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+    const result = Object.entries(obj).filter(([key]) => !fields.includes(key));
+    return Object.fromEntries(result);
 };
