cbc939d57e672b896f91917502f95be9d56e6809
elzorich <elenazorich@gmail.com>
2026-06-02 15:19:49 +0200

    trimSymbols & uniq functions

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,40 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+    if (size === undefined) {
+        return string;
+    }
+    const arr = string.split('');
+    let count = 1;
+    let result = '';
+    let lastSymbol = '';
+    for (const symbol of arr) {
+        if (symbol === lastSymbol) {
+            count++;
+        } else {
+            count = 1;
+            lastSymbol = symbol;
+        }
+        if (count <= size) {
+            result += symbol;
+        }
+    }
+    return result;
 
+}
+
+function trimSymbols2(string, size) {
+    if (size === undefined) {
+        return string;
+    }
+    const arr = string.split('');
+
+    return arr.reduce((acc, symbol) => {
+        const count = symbol === acc.lastSymbol ? acc.count +1 : 1;
+        return {
+            lastSymbol: symbol,
+            count,
+            result: count <= size ? acc.result + symbol : acc.result
+        }
+    }, { result: '', lastSymbol: '', count: 0 }).result;
 }
--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,5 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
-
+    return arr ? [...new Set(arr)] : [];
 }
